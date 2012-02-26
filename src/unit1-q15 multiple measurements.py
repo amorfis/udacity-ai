15f2f8c1550edb8ed74504a1f914d0715ff6389c
Paweł Stawicki <pawelstawicki@gmail.com>
2012-02-26 17:41:42 +0100

    u1q15 multiple measurements

---
+++ src/unit1-q15 multiple measurements.py
@@ -0,0 +1,29 @@
+#Modify the code so that it updates the probability twice
+#and gives the posterior distribution after both 
+#measurements are incorporated. Make sure that your code 
+#allows for any sequence of measurement of any length.
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i, prob in enumerate(p):
+        for mesaurement in Z:
+            hit = (mesaurement == world[i])
+            multiplier = (hit * pHit + (1 - hit) * pMiss)
+            prob = prob * multiplier
+
+        q.append(prob)
+
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+
+    return q
+
+    
+print sense(p, measurements)
